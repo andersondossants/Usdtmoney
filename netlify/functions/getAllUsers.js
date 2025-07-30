94d333547720cfe021cc6787c5d7bd9378c37167
andersondossants <142614176+andersondossants@users.noreply.github.com>
2025-07-30 14:33:29 +0100

    Update getAllUsers.js

--- netlify/functions/getAllUsers.js
+++ netlify/functions/getAllUsers.js
@@ -12,7 +12,7 @@ exports.handler = async function(event, context) {
   }
 
   const client = new Client({
-    connectionString: process.env.NETLIFY_DATABASE_URL,
+    connectionString: process.env.NETLIFY_DATABASE_URI,
     ssl: { rejectUnauthorized: false }
   });
 
